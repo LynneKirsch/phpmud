75a894dbd228c99e0c954582b51641ae6a5d7436
Lynne Kirsch <Lynne Kirsch>
2017-02-19 22:33:47 -0500

    say sends to clients

--- src/Communication.php
+++ src/Communication.php
@@ -4,6 +4,16 @@ class Communication extends GameInterface
 {
 	function doSay($args)
 	{
+		global $clients;
+		
 		$this->ch->send("\nYou say '" . $args . "'\n");
+		
+		foreach($clients as $client)
+		{
+			if($client != $this->ch)
+			{
+				$client->send($this->ch->name . "says '" . $args . "'\n");
+			}
+		}
 	}
 }
